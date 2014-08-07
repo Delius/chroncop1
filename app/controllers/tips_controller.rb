01f67909d9715555e972e561db217c61b9a36004
delius <pawel@fibrotrust.org>
2014-08-07 09:52:29 +0100

    created assotiations tips and cat

--- app/controllers/tips_controller.rb
+++ app/controllers/tips_controller.rb
@@ -1,14 +1,78 @@
-class TipsController < InheritedResources::Base
-	
-	before_filter :authenticate_user!, only: [:new, :edit, :create,:update, :destroy]
+class TipsController < ApplicationController
+  before_action :set_tip, only: [:show, :edit, :update, :destroy]
+  before_action :authenticate_user!, only: [:new, :edit, :create, :update, :destroy]
+  # GET /tips
+  # GET /tips.json
+  def index
+    if params[:sort_by].blank?
+      @tips = Tip.all
+    else
+      @tips = Tip.where(params[:sort_by])
+    end
+  end
 
-	
+  # GET /tips/1
+  # GET /tips/1.json
+  def show
+  end
 
-def tip_params
-  params.require(:tip).permit(:title,:difficulty_level,:condition_name_id, :symptom_name_id,:tip_type_id,:what_needed,:my_tip)
-end
+  # GET /tips/new
+  def new
+    @tip = Tip.new
+  end
 
+  # GET /tips/1/edit
+  def edit
+  end
 
+  # POST /tips
+  # POST /tips.json
+  def create
+    @tip = Tip.new(tip_params)
 
+    respond_to do |format|
+      if @tip.save
+        format.html { redirect_to @tip, notice: 'Tip was successfully created.' }
+        format.json { render :show, status: :created, location: @tip }
+      else
+        format.html { render :new }
+        format.json { render json: @tip.errors, status: :unprocessable_entity }
+      end
+    end
+  end
 
+  # PATCH/PUT /tips/1
+  # PATCH/PUT /tips/1.json
+  def update
+    respond_to do |format|
+      if @tip.update(tip_params)
+        format.html { redirect_to @tip, notice: 'Tip was successfully updated.' }
+        format.json { render :show, status: :ok, location: @tip }
+      else
+        format.html { render :edit }
+        format.json { render json: @tip.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  # DELETE /tips/1
+  # DELETE /tips/1.json
+  def destroy
+    @tip.destroy
+    respond_to do |format|
+      format.html { redirect_to tips_url, notice: 'Tip was successfully destroyed.' }
+      format.json { head :no_content }
+    end
+  end
+
+  private
+    # Use callbacks to share common setup or constraints between actions.
+    def set_tip
+      @tip = Tip.find(params[:id])
+    end
+
+    # Never trust parameters from the scary internet, only allow the white list through.
+    def tip_params
+      params.require(:tip).permit(:title, :difficulty_level, :condition_name_id, :symptom_name_id, :tip_type_id, :what_needed, :my_tip)
+    end
 end
