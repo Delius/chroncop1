a9bf9fc8ddf3a01badb7c73d2d51cbc338fb9c9f
delius <pawel@fibrotrust.org>
2014-08-11 19:49:30 +0100

    added social buttons

--- app/models/tip.rb
+++ app/models/tip.rb
@@ -15,28 +15,7 @@ DIFFICULTY=%w(Easy Medium Hard)
 
 
  
-def tweet!
-tweet_desc = @tip.title
-shrunk_url = Tip.tiny_url(134 - tweet_desc.length, url)
-if tweet_desc.length > 134 - shrunk_url.length
-tweet_desc = tweet_desc[0...(134 - shrunk_url.length)] + '...'
-end
-tweet = "#{tweet_desc} #{shrunk_url}"
-Twitter.update tweet
-update_attribute(:tweeted_at, Time.now.utc)
-end
- 
-def self.tiny_url(available_length, url)
-return url if url.length < available_length
-string = "http://is.gd/api.php?longurl=" + CGI::escape(url)
-open(string).read.strip
-rescue StandardError => e
-puts "Error in tiny_url: #{e.message}\n#{e.backtrace}"
-end
 
-
-
-#---------------------------------------------------------------------------------------
 def all_tags
     tags.map(&:name).join(", ")
   end
