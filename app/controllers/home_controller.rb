a8d6503b0afb973297db9eee729336c178dea689
delius <pawel@fibrotrust.org>
2014-08-11 09:42:59 +0100

    updated twitter auth

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,10 +1,10 @@
 class HomeController < ApplicationController
 
 	client = Twitter::REST::Client.new do |config|
-		config.consumer_key= "Bdyx2U1waoNMEXn78nGdd14t2"
-		config.consumer_secret="0YPMNPd7V4p36Zzj5xzBfweVqPS4IhOdlE99Txun1eW2giGO6C"
-		config.access_token= "168429845-WRHvWvXos3b9wcqsiPRHCCdJVvnHaJetdU4YbIDr"
-		config.access_token_secret = "FTlz8R1BkRhN7jzL1kwoSX6TytmTrTs2lhsn8v2Semizm"
+		config.consumer_key= ENV["TWITTER_KEY"]
+		config.consumer_secret= ENV["TWITTER_SECRET"]
+		config.access_token= ENV["TWITTER_TOKEN"]
+		config.access_token_secret = ENV["TWITTER_TOKEN_SECRET"]
 		end
 		@friends = client.friends.take(20)
 
--- config/initializers/twitter.rb
+++ config/initializers/twitter.rb
@@ -1,6 +1,7 @@
 client = Twitter::REST::Client.new do |config|
-    config.consumer_key= "Bdyx2U1waoNMEXn78nGdd14t2"
-    config.consumer_secret="0YPMNPd7V4p36Zzj5xzBfweVqPS4IhOdlE99Txun1eW2giGO6C"
-    config.access_token= "168429845-WRHvWvXos3b9wcqsiPRHCCdJVvnHaJetdU4YbIDr"
-    config.access_token_secret = "FTlz8R1BkRhN7jzL1kwoSX6TytmTrTs2lhsn8v2Semizm"
+		config.consumer_key= ENV["TWITTER_KEY"]
+		config.consumer_secret= ENV["TWITTER_SECRET"]
+		config.access_token= "168429845-anokGzKl2dUM4iTpN44KfTpVmtvZGQlJMUt6hqZT"
+		config.access_token_secret = "gQEWaSpTfRtvWdXwhqCXzsiKZYTihIh23j0skbo5tzT73"
+
     end
