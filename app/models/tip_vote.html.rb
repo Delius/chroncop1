b005977069beb43ef0b22c7dd22bb4d096d2b9d7
Delius <pawel@fibrotrust.org>
2014-09-08 03:13:55 +0100

    heroku fix ?

--- app/models/tip_vote.html.rb
+++ app/models/tip_vote.html.rb
@@ -1,6 +1,6 @@
 # validates_uniqueness_of :tip_id, scope: :user_id
 # validates_inclusion_of :value, in: [1, -1]
-validate :ensure_not_author
+# validate :ensure_not_author
 
 def ensure_not_author
   errors.add :user_id, "is the author of the tip" if tip.user_id == user_id
