14bc7b1fdeb2383f4e61c5fb8fbf4a51984ec7a9
Delius <pawel@fibrotrust.org>
2014-09-09 21:50:39 +0100

    add follow button only if user can follow

--- app/models/user.rb
+++ app/models/user.rb
@@ -23,17 +23,20 @@ class User < ActiveRecord::Base
   has_many :shouts
   
   has_many :followed_user_relationships,
-    foreign_key: :follower_id,
-    class_name: 'FollowingRelationship'
+  foreign_key: :follower_id,
+  class_name: 'FollowingRelationship'
   has_many :followed_users, through: :followed_user_relationships
 
   has_many :follower_relationships, 
-    foreign_key: :followed_user_id, 
-    class_name: 'FollowingRelationship'
+  foreign_key: :followed_user_id, 
+  class_name: 'FollowingRelationship'
   has_many :follower_relationships, through: :follower_relationships
 
   def following? user
     followed_user_ids.include? user.id
+  end
+    def can_follow? user 
+    self != user
   end
 
   def follow user
