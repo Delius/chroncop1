e0f4d3859e474f4cb57ec60a0716f1c96e458623
Delius <pawel@fibrotrust.org>
2014-09-09 14:51:25 +0100

    add shouts helper gravatar

---
+++ app/helpers/xxx.rb
@@ -0,0 +1,16 @@
+# require 'digest/md5'
+
+# module UsersHelper
+# 	def gravatar user, size=48
+# 		digest = Digest::MD5.hexdigest(@user.email)
+# 		image_tag("http://gravatar.com/avatar/#{digest}?s=#{size}")
+# 	end
+
+# 	# def follow_button user
+# 	# 	if current_user.following? user 
+# 	# 		button_to 'Unfollow', user_follow_path(user), method: :delete
+# 	# 	else
+# 	# 		button_to 'Follow', user_follow_path(user)
+# 	# 	end
+# 	# end
+# end
