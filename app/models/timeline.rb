07cd0caab115a549e656e21a647ef79fe0abcc20
Delius <pawel@fibrotrust.org>
2014-09-09 20:20:34 +0100

    add dashboard object

--- app/models/timeline.rb
+++ app/models/timeline.rb
@@ -1,17 +1,24 @@
 class Timeline
-	# extend ActiveModel::Naming
+	# extend ActiveModel::Naming /// 
+	# not compatible must be partial_path
 
 	def initialize user
 		@user = user
 	end
 
 	def shouts
-		@user.shouts
+		Shout.where(user_id: shout_user_ids)
 	end
 
 
 	def to_partial_path
 		"timelines/timeline"
 	end
+
+private
+
+def shout_user_ids
+	[@user.id] + @user.followed_user_ids
+end
 
 end
