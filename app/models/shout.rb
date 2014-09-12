1471ce25b48af133215e6e335e7485f38da26b07
Delius <pawel@fibrotrust.org>
2014-09-12 12:00:57 +0100

    styled shout view

--- app/models/shout.rb
+++ app/models/shout.rb
@@ -2,24 +2,24 @@ class Shout < ActiveRecord::Base
 	belongs_to :user
 	default_scope { order("created_at DESC")}
 	belongs_to :content, polymorphic: true
-
+	
 	def self.text_shouts
 		where(content_type: 'TextShout')
 	end
 
-	searchable do
-		text :content do
-			content.index
-		end
-	end
+	# searchable do
+	# 	text :content do
+	# 		content.index
+	# 	end
+	# end
 
 	def to_partial_path
 		"shouts/shout"
 	end
 
 # moved to search.rb
-  # def self.search term
-  # 	text_shouts = TextShout.where("body LIKE ?", "%#{term}%") #ILIKE for postgres
-  # 	where(content_type: 'TextShout', content_id: text_shouts)
-  # end
+  def self.search term
+  	text_shouts = TextShout.where("body LIKE ?", "%#{term}%") #ILIKE for postgres
+  	where(content_type: 'TextShout', content_id: text_shouts)
+  end
 end
