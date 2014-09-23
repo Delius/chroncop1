74c2670f3573c8458d960e92ffd467182698459c
Delius <pawel@fibrotrust.org>
2014-09-23 18:11:30 +0100

    password confirmation

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -14,7 +14,7 @@ SociaLoginRails::Application.configure do
   config.action_controller.perform_caching = false
 
   # Don't care if the mailer can't send.
-  config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.raise_delivery_errors = true
 
   # Print deprecation notices to the Rails logger.
   config.active_support.deprecation = :log
@@ -31,12 +31,6 @@ SociaLoginRails::Application.configure do
  # Prepend all log lines with the following tags.
   # config.log_tags = [ :subdomain, :uuid ]
 
-  config.app_domain = 'localhost:3000'
-  Rails.application.routes.default_url_options[:host] = 'localhost:3000'
-  # Email
-  #config.action_mailer.delivery_method = :smtp
-  #config.action_mailer.perform_deliveries = true
-  #config.action_mailer.default_url_options = { host: config.app_domain }
   config.action_mailer.default_url_options = { :host => 'localhost:3000' }
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.perform_deliveries = true
