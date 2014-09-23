2c37676e356c8aab546bbbd1bfa13a79c8c630dc
Delius <pawel@fibrotrust.org>
2014-09-23 20:47:56 +0100

    password confirmation

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -48,7 +48,7 @@ SociaLoginRails::Application.configure do
   # Prepend all log lines with the following tags.
   # config.log_tags = [ :subdomain, :uuid ]
 
-  config.action_mailer.default_url_options = { :host => 'localhost:3000' }
+  config.action_mailer.default_url_options = { :host => 'chronicillnesses.co.uk' }
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.perform_deliveries = true
   config.action_mailer.raise_delivery_errors = false
