a3cc7dece36a348c76f28a44fbb848686dc0389d
Delius <pawel@fibrotrust.org>
2014-09-23 18:29:00 +0100

    password confirmation

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -48,7 +48,7 @@ SociaLoginRails::Application.configure do
   # Prepend all log lines with the following tags.
   # config.log_tags = [ :subdomain, :uuid ]
 
-   config.action_mailer.default_url_options = { :host => 'thawing-lake-7709.herokuapp.com' }
+   config.action_mailer.default_url_options = { :host => 'host=polar-sea-7238.herokuapp.com' }
 
 
   #These settings are for the sending out email for active admin and consequently the   devise mailer
