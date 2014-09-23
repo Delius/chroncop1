5d36a6035258f05f2eaba6de089f86186ec41350
Delius <pawel@fibrotrust.org>
2014-09-23 20:09:57 +0100

    password confirmation

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -52,15 +52,14 @@ SociaLoginRails::Application.configure do
   config.action_mailer.default_url_options = { :host => 'chronicillnesses.co.uk' }
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.perform_deliveries = true
-  
+  config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.default :charset => "utf-8"
   config.action_mailer.smtp_settings = {
     address: 'auth.smtp.1and1.co.uk', 
     port: '25',
     domain: 'chronicillnesses.co.uk',
     user_name: 'support@chronicdiseases.org.uk',
     password: '22Chaberek22',
-  
-    
 
 
   } 
