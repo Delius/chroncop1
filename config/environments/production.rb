afeb3b81d8db310545648912a1dfd4632a41e42d
Delius <pawel@fibrotrust.org>
2014-09-29 17:47:23 +0100

    aws setup for heroku images

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -22,7 +22,7 @@ SociaLoginRails::Application.configure do
 
 config.paperclip_defaults = {
   :storage => :s3,
-  s3_host_name: => ['s3-eu-west-1.amazonaws.com'],
+  :s3_host_name => ['s3-eu-west-1.amazonaws.com'],
   :s3_credentials => {
     :bucket => ENV['AWS_BUCKET'],
     :access_key_id => ENV['AWS_KEY'],
